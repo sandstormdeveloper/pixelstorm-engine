03de83de63b3ec74c9a4a11b252bb55203297892
Sandstorm <57902127+sandstormdeveloper@users.noreply.github.com>
2026-05-02 21:16:28 +0200

    Update Entity.h

--- engine/include/pixelstorm/ecs/Entity.h
+++ engine/include/pixelstorm/ecs/Entity.h
@@ -14,6 +14,18 @@ public:
     EntityId GetId() const; // Returns entity identifier
     bool IsValid() const;   // Confirms if entity was created by registry
 
+    template <typename T, typename... Args>
+    T &AddComponent(Args &&...args); // Adds component to entity
+
+    template <typename T>
+    bool HasComponent() const; // Checks if component exists
+
+    template <typename T>
+    T &GetComponent(); // Returns component
+
+    template <typename T>
+    void RemoveComponent(); // Removes component
+
 private:
     Entity(EntityId id, Registry *registry); // Constructor used by registry
 
@@ -22,3 +34,27 @@ private:
 
     friend class Registry; // Allows registry to access private constructor
 };
+
+template <typename T, typename... Args>
+T &Entity::AddComponent(Args &&...args)
+{
+    return m_Registry->AddComponent<T>(*this, std::forward<Args>(args)...);
+}
+
+template <typename T>
+bool Entity::HasComponent() const
+{
+    return m_Registry->HasComponent<T>(*this);
+}
+
+template <typename T>
+T &Entity::GetComponent()
+{
+    return m_Registry->GetComponent<T>(*this);
+}
+
+template <typename T>
+void Entity::RemoveComponent()
+{
+    m_Registry->RemoveComponent<T>(*this);
+}
